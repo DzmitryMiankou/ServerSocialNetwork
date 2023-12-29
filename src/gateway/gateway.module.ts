f4cac815698f2188de4e47501786870460422360
DzmitryMiankou <gmiankou@gmail.com>
2023-12-29 22:23:50 +0300

    recc

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GatewayService } from './gateway.service';
+import { JwtModule } from '@nestjs/jwt';
+import { config } from 'src/config/config';
 
 @Module({
+  imports: [JwtModule.registerAsync({ ...config })],
   providers: [GatewayService],
 })
 export class GatewayModule {}
--- src/gateway/gateway.service.ts
+++ src/gateway/gateway.service.ts
@@ -1,22 +1,56 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import {
   WebSocketGateway,
   SubscribeMessage,
   MessageBody,
   ConnectedSocket,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
-import { Socket } from 'dgram';
+import { Namespace, Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class GatewayService {
+export class GatewayService
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(
+    private JWT: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @WebSocketServer()
+  io: Namespace;
+
+  handleDisconnect(socket: Socket) {}
+  handleConnection(client: Socket) {
+    console.log(client.id);
+    try {
+      const access_token =
+        client.handshake.auth.Authorization.replace('Bearer=', '') ?? null;
+
+      if (!access_token) return this.handleDisconnect(client);
+
+      this.JWT.verify(access_token, {
+        secret: this.configService.get<string>(`SECRET_ACCESS_KEY`),
+      });
+    } catch (error) {
+      this.io.use((socket, next) => {
+        const err: any = new Error('not authorized');
+        err.data = { content: 'Please retry later' };
+        next(err);
+      });
+    }
+  }
+
   @SubscribeMessage(`all_messages`)
   message(@MessageBody() id: string, @ConnectedSocket() socket: Socket) {
-    console.log(id);
-    socket.emit('all_messages', { id: 1, name: id });
+    socket.emit('all_messages', { id: 1, name: id ?? 'hi' });
   }
 
   @SubscribeMessage(`send_message`)
   sendMessage(@MessageBody() id: string, @ConnectedSocket() socket: Socket) {
-    console.log(id);
     socket.emit(`all_messages`, { id: 1, name: id });
   }
 }
