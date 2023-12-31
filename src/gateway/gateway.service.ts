052a733c3d299b5a8f5ad79af4aa38a66bce911c
DzmitryMiankou <gmiankou@gmail.com>
2023-12-31 23:49:43 +0300

    фу

--- src/gateway/gateway.service.ts
+++ src/gateway/gateway.service.ts
@@ -11,6 +11,19 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
+interface Message {
+  timeSent: string;
+  message: string;
+}
+
+export interface ServerToClientEvents {
+  chat: (e: Message) => void;
+}
+
+export interface ClientToServerEvents {
+  chat: (e: Message) => void;
+}
+
 @WebSocketGateway({
   cors: {
     origin: 'http://localhost:3000',
@@ -25,39 +38,53 @@ export class GatewayService
   ) {}
 
   @WebSocketServer()
-  io: Server;
+  io: Server = new Server<ServerToClientEvents, ClientToServerEvents>();
 
-  handleDisconnect(sockt: Socket) {
-    console.log(`Disconnect: ${sockt.id}`);
+  handleDisconnect(socket: Socket) {
+    console.log(`Disconnect: ${socket.id}`);
+
+    this.io.use((socket, next) => {
+      const err: any = new Error('not authorized');
+      err.data = { content: 'Please retry later' };
+      next();
+    });
   }
 
   handleConnection(client: Socket) {
-    console.log(client.handshake.auth);
     try {
       const access_token =
         client.handshake.auth.Authorization.replace('Bearer=', '') ?? null;
-
       if (!access_token) return this.handleDisconnect(client);
-      this.JWT.verify(access_token, {
+
+      return this.JWT.verify(access_token, {
         secret: this.configService.get<string>(`SECRET_ACCESS_KEY`),
       });
     } catch (error) {
-      this.io.use((socket, next) => {
-        const err: any = new Error('not authorized');
-        err.data = { content: 'Please retry later' };
-        next(err);
-      });
+      this.handleDisconnect(client);
     }
   }
 
   @SubscribeMessage(`all_messages`)
   message(@MessageBody() id: string, @ConnectedSocket() socket: Socket) {
-    socket.emit('all_messages', { id: 1, name: id ?? 'hi' });
+    const messages = [
+      {
+        timeSent: new Date(Date.now()).toLocaleString('en-US'),
+        message: 'Hi, my name is Silver.',
+      },
+      {
+        timeSent: new Date(Date.now()).toLocaleString('en-US'),
+        message: 'Hello. How are you?',
+      },
+    ];
+    socket.emit('all_messages', messages);
   }
 
   @SubscribeMessage(`send_message`)
-  sendMessage(@MessageBody() id: string, @ConnectedSocket() socket: Socket) {
-    socket.emit(`all_messages`, { id: 1, name: id });
+  sendMessage(
+    @MessageBody() message: Message,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    socket.emit(`send_message`, message);
   }
 
   @SubscribeMessage(`click`)
